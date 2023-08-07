f1282aff91fca00cfc79226ed615350d56aecc1f
Kitae's Github <aqua0405@gmail.com>
2023-08-08 01:19:56 +0900

    turtle

---
+++ turtle/turtle.js
@@ -0,0 +1,38 @@
+
+
+// showGrid(20);      
+// forward(distance)  
+// right(angle)       
+// left(angle) 	   
+// goto(x,y) 	       
+// clear() 	       
+// penup() 	       
+// pendown() 	       
+// reset() 	       
+// angle(angle)	   
+// width(width)       
+
+// color(r,g,b)
+// color([r,g,b])
+// color("red")
+//color("#ff0000")
+
+
+
+// showGrid(50); 
+// setSpeed(500);
+
+// width(5);
+// color("blue");
+// forward(120);
+
+width(1);
+left(65);
+color("red");
+forward(150);
+
+goto(184,-208);
+width(2);
+right(100);
+color("green");
+forward(150);
